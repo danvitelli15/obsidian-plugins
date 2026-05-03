3c6938d78a962ad2f8cc8ec4a3a7b085170bde8d
Daniel Vitelli <danvitelli15@gmail.com>
2026-05-03 16:34:04 -0500

    got teh extension opening the file correctly

--- .obsidian/plugins/webvtt-viewer/main.ts
+++ .obsidian/plugins/webvtt-viewer/main.ts
@@ -1,13 +1,72 @@
-import { Notice, Plugin } from "obsidian";
+import { Plugin, TextFileView, WorkspaceLeaf } from "obsidian";
+import { deserializeVtt } from "./vtt";
+
+export const VIEW_TYPE_WEBVTT = "webvtt-viewer";
+
+export class WebVTTViewer extends TextFileView {
+  private _data: string = "";
+
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
+
+  clear(): void {
+    this._data = "";
+  }
+
+  getDisplayText(): string {
+    if (this.file) {
+      return this.file.name;
+    } else {
+      return "WebVTT Viewer";
+    }
+  }
+
+  getViewData(): string {
+    return this._data;
+  }
+
+  getViewType(): string {
+    return VIEW_TYPE_WEBVTT;
+  }
+
+  async onOpen() {
+    this.contentEl.empty();
+    this.renderContent(this.contentEl);
+  }
+
+  setViewData(data: string, clear: boolean): void {
+    if (clear) {
+      this.clear();
+    }
+    this._data = data;
+    this.renderContent(this.contentEl);
+  }
+
+  private renderCaption(caption: string, parent: HTMLElement) {
+    parent.createEl("p", { text: caption });
+  }
+
+  private renderContent(parent: HTMLElement) {
+    parent.empty();
+
+    const captions = this._data.split("\n\n");
+
+    const contentDiv = parent.createEl("div");
+
+    captions.forEach((caption) => {
+      this.renderCaption(caption, contentDiv);
+    });
+  }
+}
 
 export default class WebVTTViewerPlugin extends Plugin {
   async onload() {
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-      new Notice("Click");
-    });
+    this.registerView(VIEW_TYPE_WEBVTT, (leaf) => new WebVTTViewer(leaf));
+    this.registerExtensions(["vtt"], VIEW_TYPE_WEBVTT);
   }
 
-  onunload() {}
+  onunload() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_WEBVTT);
+  }
 }
